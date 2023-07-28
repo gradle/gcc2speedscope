378f6e9d2ebc9ca8df257d263c5a917231c32996
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2023-07-28 15:54:26 -0300

    Avoid memory starvation by aggregating events using external storage
    More specifically, an H2 database is now used for the aggregation events.
    
    Processing is now parallelized among 3 coroutines:
    - the parser: scans the Gradle debug log for matching events
    - the aggregator: aggregates the events into profiles
    - the writer: outputs the final json for the aggregates

--- src/test/kotlin/gcc2speedscope/AppTest.kt
+++ src/test/kotlin/gcc2speedscope/AppTest.kt
@@ -1,30 +1,38 @@
 package gcc2speedscope
 
-import java.io.BufferedReader
-import java.io.StringWriter
+import org.junit.jupiter.api.io.TempDir
+import java.io.Reader
+import java.nio.file.Path
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class AppTest {
 
     @Test
-    fun `produces expected json`() {
-        val speedscopeDocument = speedscopeDocumentFor(bufferedReaderForResource("/debug.log"))
+    fun `produces expected json`(@TempDir tempDir: Path) {
+        val outputFile = tempDir.resolve("actual.json")
+        writeSpeedscopeDocumentFor(
+            bufferedReaderForResource("/debug.log"),
+            outputFile,
+            prettyPrint = true
+        )
+
         // uncomment to update expected file
         // java.io.File("./src/test/resources/expected.json").writeText(speedscopeDocument)
         assertEquals(
             bufferedReaderForResource("/expected.json").readText(),
-            speedscopeDocument
+            outputFile.toFile().readText()
         )
     }
 
     private
-    fun speedscopeDocumentFor(debugLog: BufferedReader): String = StringWriter().run {
-        writeSpeedscopeDocumentFor(parseConfigurationCacheDebugLog(debugLog), prettyPrint = true)
-        toString()
+    fun writeSpeedscopeDocumentFor(debugLogReader: Reader, outputFile: Path, prettyPrint: Boolean = false) {
+        outputFile.toFile().bufferedWriter().use { writer ->
+            writeSpeedscopeDocumentTo(writer, debugLogReader, prettyPrint, "$outputFile.db")
+        }
     }
 
     private
     fun bufferedReaderForResource(resource: String) =
-        javaClass.getResourceAsStream(resource).bufferedReader()
+        javaClass.getResourceAsStream(resource)!!.bufferedReader()
 }
